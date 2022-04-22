6db06e3b255cb6acbc35e931a078c3ce6b949dc5
gurchunov-1997@mail.ru <Flatronl1718s>
2022-04-22 22:08:26 +0300

    Task1

--- app/src/main/java/com/example/tms/MainActivity.kt
+++ app/src/main/java/com/example/tms/MainActivity.kt
@@ -4,8 +4,27 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    var usersList: MutableList<User> = mutableListOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        addUser("вася", "иванов", 15)
+        addUser("петя", "смирнов", 16)
+        addUser("коля", "соболев", 17)
+
+        output()
+    }
+
+    private fun addUser(name: String, secondName: String, age: Int) {
+        val user = User(name, secondName, age)
+        usersList.add(user)
+    }
+
+    private fun output() {
+        for (user in usersList) {
+            println(user)
+        }
     }
 }
---
+++ app/src/main/java/com/example/tms/User.kt
@@ -0,0 +1,3 @@
+package com.example.tms
+
+data class User(val name: String, val secondName: String, val age: Int)
