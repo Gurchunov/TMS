b8b421fef99e2faa0ca06562c9519a4803f09083
gurchunov-1997@mail.ru <Flatronl1718s>
2022-04-23 12:18:35 +0300

    Task2

--- app/src/main/java/com/example/tms/MainActivity.kt
+++ app/src/main/java/com/example/tms/MainActivity.kt
@@ -2,29 +2,33 @@ package com.example.tms
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
-    var usersList: MutableList<User> = mutableListOf()
+    private val arrayQuestions: Array<String> = arrayOf(
+        "Я устроюсь на работу?",
+        "Меня ждет успех?"
+    )
+
+    private val arrayAnswers: Array<String> = arrayOf(
+        "Да",
+        "Нет",
+        "Возможно",
+        "Скорее всего",
+        "Имееются перспективы",
+        "Вопрос задан не верно"
+    )
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        addUser("вася", "иванов", 15)
-        addUser("петя", "смирнов", 16)
-        addUser("коля", "соболев", 17)
-
         output()
     }
 
-    private fun addUser(name: String, secondName: String, age: Int) {
-        val user = User(name, secondName, age)
-        usersList.add(user)
-    }
-
-    private fun output() {
-        for (user in usersList) {
-            println(user)
-        }
+    fun output() {
+        println(arrayQuestions.random())
+        println(arrayAnswers.random())
     }
 }
--- app/src/main/java/com/example/tms/User.kt
+++
@@ -1,3 +0,0 @@
-package com.example.tms
-
-data class User(val name: String, val secondName: String, val age: Int)
