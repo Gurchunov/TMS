a0e1b616a7450a3805add87be8e4ea451e480e40
gurchunov-1997@mail.ru <Flatronl1718s>
2022-04-24 06:33:45 +0300

    Task1

--- app/src/main/java/com/example/tms/MainActivity.kt
+++ app/src/main/java/com/example/tms/MainActivity.kt
@@ -4,16 +4,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-    var usersList: MutableList<User> = mutableListOf()
+    private var usersList: MutableList<User> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        addUser("вася", "иванов", 15)
         addUser("петя", "смирнов", 16)
-        addUser("коля", "соболев", 17)
+        addUser("вася", "иванов", 15)
+        addUser("коля", "соболев", 18)
 
+        output()
+        sortList(usersList)
+        output()
+        deleteUser(usersList)
         output()
     }
 
@@ -26,5 +30,16 @@ class MainActivity : AppCompatActivity() {
         for (user in usersList) {
             println(user)
         }
+    }
+
+    private fun sortList(usersList: MutableList<User>): MutableList<User> {
+        usersList.sortWith(
+            compareBy(String.CASE_INSENSITIVE_ORDER) { it.name })
+        return usersList
+    }
+
+    private fun deleteUser(usersList: MutableList<User>): MutableList<User> {
+        usersList.removeIf { user -> user.age < 18 }
+        return usersList
     }
 }
