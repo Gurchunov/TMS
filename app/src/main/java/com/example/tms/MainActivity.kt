61d971803120f45911f7f2d4f2a85ccb5b0c7c5d
gurchunov-1997@mail.ru <Flatronl1718s>
2022-04-29 00:00:56 +0300

    Task5

--- app/src/main/java/com/example/tms/MainActivity.kt
+++ app/src/main/java/com/example/tms/MainActivity.kt
@@ -7,16 +7,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        max(1, 2, 3)
+        numberOfEvenNumbers()
     }
 
-    private fun max(a: Int, b: Int, c: Int) {
-        val plus = a + b + c
-        val multiplay = a * b * c
-        when {
-            plus == multiplay -> println("значения равны")
-            plus > multiplay -> println("$plus > $multiplay")
-            plus < multiplay -> println("$plus < $multiplay")
+    private fun numberOfEvenNumbers() {
+        var count = 0
+        for (x in 1..99) {
+            if (x % 2 == 0) {
+                count++
+            }
         }
+        println(count)
     }
 }
