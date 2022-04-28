cb2bc63930fe353e015de26dc8697494ff4a2da9
gurchunov-1997@mail.ru <Flatronl1718s>
2022-04-28 18:13:49 +0300

    Task1

--- app/src/main/java/com/example/tms/MainActivity.kt
+++ app/src/main/java/com/example/tms/MainActivity.kt
@@ -7,5 +7,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        isEvenA(3)
+    }
+    private fun isEvenA(a: Int) {
+        val b = 5
+        when (a % 2 == 0) {
+            true -> println("число A четное, a * b = "+ a * b)
+            false -> println("число A нечетное, a + b = "+ (a + b))
+        }
     }
 }
