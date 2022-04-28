517c09c96b494268f68ee7a268737c788b1f6790
gurchunov-1997@mail.ru <Flatronl1718s>
2022-04-28 23:48:38 +0300

    Task2

--- app/src/main/java/com/example/tms/MainActivity.kt
+++ app/src/main/java/com/example/tms/MainActivity.kt
@@ -7,18 +7,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        studentGrade(5)
+        max(1, 2, 3)
     }
-    private fun studentGrade(a: Int) {
-        val message = "ваша оценка"
-        when (a) {
-            in 0..19 -> println("$message F")
-            in 20..39 -> println("$message E")
-            in 40..59 -> println("$message D")
-            in 60..74 -> println("$message C")
-            in 75..89 -> println("$message B")
-            in 90..100 -> println("$message A")
-            else -> println("такой оценки нет")
+
+    private fun max(a: Int, b: Int, c: Int) {
+        val plus = a + b + c
+        val multiplay = a * b * c
+        when {
+            plus == multiplay -> println("значения равны")
+            plus > multiplay -> println("$plus > $multiplay")
+            plus < multiplay -> println("$plus < $multiplay")
         }
     }
 }
