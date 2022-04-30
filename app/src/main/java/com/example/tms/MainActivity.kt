0079e3636ff375656029285a5538614e8a69ca3d
gurchunov-1997@mail.ru <Flatronl1718s>
2022-04-30 08:46:46 +0300

    add all method

--- app/src/main/java/com/example/tms/MainActivity.kt
+++ app/src/main/java/com/example/tms/MainActivity.kt
@@ -8,6 +8,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         numberOfEvenNumbers()
+        isEvenA(3)
+        studentGrade(5)
+        max(1, 2, 3)
     }
 
     private fun numberOfEvenNumbers() {
@@ -18,5 +21,36 @@ class MainActivity : AppCompatActivity() {
             }
         }
         println(count)
+    }
+
+    private fun isEvenA(a: Int) {
+        val b = 5
+        when (a % 2 == 0) {
+            true -> println("число A четное, a * b = " + a * b)
+            false -> println("число A нечетное, a + b = " + (a + b))
+        }
+    }
+
+    private fun studentGrade(a: Int) {
+        val message = "ваша оценка"
+        when (a) {
+            in 0..19 -> println("$message F")
+            in 20..39 -> println("$message E")
+            in 40..59 -> println("$message D")
+            in 60..74 -> println("$message C")
+            in 75..89 -> println("$message B")
+            in 90..100 -> println("$message A")
+            else -> println("такой оценки нет")
+        }
+    }
+
+    private fun max(a: Int, b: Int, c: Int) {
+        val plus = a + b + c
+        val multiplay = a * b * c
+        when {
+            plus == multiplay -> println("значения равны")
+            plus > multiplay -> println("$plus > $multiplay")
+            plus < multiplay -> println("$plus < $multiplay")
+        }
     }
 }
