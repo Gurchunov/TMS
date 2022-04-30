beed4aa787388efaf0e2ae8e5b2e3e5cd072a08a
gurchunov-1997@mail.ru <Flatronl1718s>
2022-04-30 23:41:48 +0300

    сделал счетчик в миллисекундах

--- app/src/main/java/com/example/tms/MainActivity.kt
+++ app/src/main/java/com/example/tms/MainActivity.kt
@@ -2,10 +2,41 @@ package com.example.tms
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import com.example.tms.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var timer: CountDownTimer? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+        binding.apply {
+            btnOnfirm.setOnClickListener {
+                startTimer( binding.edText.text.toString().toLong())
+            }
+        }
     }
+
+    private fun startTimer(time: Long) {
+        timer?.cancel()
+        timer = object : CountDownTimer(time, 1000) {
+            override fun onTick(p0: Long) {
+                binding.string.text = p0.toString()
+            }
+
+            override fun onFinish() {
+                binding.string.text = "время вышло"
+            }
+        }.start()
+
+    }
+
+
 }
