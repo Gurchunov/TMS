1b7e2da80eb628b6257ed32e6bc30118b55b333d
gurchunov-1997@mail.ru <Flatronl1718s>
2022-05-08 17:48:46 +0300

    проверка

--- app/src/main/java/com/example/tms/MainActivity.kt
+++ app/src/main/java/com/example/tms/MainActivity.kt
@@ -23,38 +23,59 @@ class MainActivity : AppCompatActivity() {
         "Имееются перспективы",
         "Вопрос задан не верно"
     )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        numberOfEvenNumbers()
+        isEvenA(3)
+        studentGrade(5)
+        max(1, 2, 3)
+    }
 
-        binding.apply {
-
-            btnOnfirm.setOnClickListener {
-                if (timer == null) {
-                    startTimer(10000)
-                } else {
-                    startTimer(edText.text.toString().toLong())
-                }
+    private fun numberOfEvenNumbers() {
+        var count = 0
+        for (x in 1..99) {
+            if (x % 2 == 0) {
+                count++
             }
+        }
+        println(count)
+    }
+
+    private fun isEvenA(a: Int) {
+        val b = 5
+        when (a % 2 == 0) {
+            true -> println("число A четное, a * b = " + a * b)
+            false -> println("число A нечетное, a + b = " + (a + b))
         }
     }
 
-    private fun startTimer(time: Long) {
-        timer?.cancel()
-        timer = object : CountDownTimer(time, 1000) {
-            override fun onTick(p0: Long) {
-                binding.string.text = p0.toString()
-            }
+    private fun studentGrade(a: Int) {
+        val message = "ваша оценка"
+        when (a) {
+            in 0..19 -> println("$message F")
+            in 20..39 -> println("$message E")
+            in 40..59 -> println("$message D")
+            in 60..74 -> println("$message C")
+            in 75..89 -> println("$message B")
+            in 90..100 -> println("$message A")
+            else -> println("такой оценки нет")
+        }
+    }
 
-            override fun onFinish() {
-                binding.string.text = "время вышло"
-            }
-        }.start()
+    private fun max(a: Int, b: Int, c: Int) {
+        val plus = a + b + c
+        val multiplay = a * b * c
+        when {
+            plus == multiplay -> println("значения равны")
+            plus > multiplay -> println("$plus > $multiplay")
+            plus < multiplay -> println("$plus < $multiplay")
+        }
 
         randomNumber(24)
     }
+
     private fun randomNumber(x: Int) {
         val range = 25..100
         if (x in 6..154) {
@@ -66,6 +87,7 @@ class MainActivity : AppCompatActivity() {
         }
         output()
     }
+
     fun output() {
         println(arrayQuestions.random())
         println(arrayAnswers.random())
