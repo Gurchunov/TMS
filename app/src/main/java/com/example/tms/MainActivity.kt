fc8774b7975ad341609e710a5778a1ad80ec951e
gurchunov-1997@mail.ru <Flatronl1718s>
2022-05-16 14:39:27 +0300

    добавил 3 флага и сделал кнопки

--- app/src/main/java/com/example/tms/MainActivity.kt
+++ app/src/main/java/com/example/tms/MainActivity.kt
@@ -1,21 +1,41 @@
 package com.example.tms
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
-
-import android.os.CountDownTimer
+import androidx.appcompat.app.AppCompatActivity
 import com.example.tms.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
 
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.apply {
+            btnBolivia.setOnClickListener {
+                val intent = Intent(applicationContext, BoliviaActivity::class.java)
+                startActivity(intent)
+            }
+            btnMaldives.setOnClickListener {
+                val intent = Intent(applicationContext, MaldiviActivity::class.java)
+                startActivity(intent)
+            }
+            btnMauritius.setOnClickListener {
+                val intent = Intent(applicationContext, MavrikiActivity::class.java)
+                startActivity(intent)
+            }
+            btnUAE.setOnClickListener {
+                val intent = Intent(applicationContext, OaeActivity::class.java)
+                startActivity(intent)
+            }
+            btnSweden.setOnClickListener {
+                val intent = Intent(applicationContext, ShveciaActivity::class.java)
+                startActivity(intent)
+            }
+        }
     }
-
-
-
-
 }
