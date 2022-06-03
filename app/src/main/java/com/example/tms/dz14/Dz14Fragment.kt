80408f1a08979e1eb4b73dafd86afe2b095ec3c1
gurchunov-1997@mail.ru <Flatronl1718s>
2022-06-03 12:34:10 +0300

    обновл=ил view model в дз 14

--- app/src/main/java/com/example/tms/dz14/Dz14Fragment.kt
+++ app/src/main/java/com/example/tms/dz14/Dz14Fragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.Observer
@@ -13,52 +14,33 @@ import com.example.tms.databinding.FragmentDz14Binding
 class Dz14Fragment : Fragment() {
     private lateinit var binding: FragmentDz14Binding
     private val viewModel: MyViewModel by viewModels()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentDz14Binding.inflate(inflater, container, false)
         binding.apply {
-            tvQuestion.text = arrayQuestions.random().toString()
-            tvAnswer.text = arrayAnswers.random().toString()
+            tvQuestion.text = viewModel.arrayQuestions
+            tvAnswer.text = viewModel.arrayAnswers
         }
         return binding.root
     }
 
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        viewModel.questionViewModel.observe(this@Dz14Fragment as LifecycleOwner, Observer {
-            binding.tvQuestion.text = it
-        })
-        viewModel.answerViewModel.observe(this@Dz14Fragment as LifecycleOwner, Observer {
-            binding.tvAnswer.text = it
-        })
         binding.apply {
-            viewModel.questionViewModel.observe(this@Dz14Fragment as LifecycleOwner, Observer {
-                tvQuestion.text = it
-            })
-            viewModel.answerViewModel.observe(this@Dz14Fragment as LifecycleOwner, Observer {
-                tvAnswer.text = it
-            })
             btnRandom.setOnClickListener {
-                viewModel.questionViewModel.value = arrayQuestions.random().toString()
-                viewModel.answerViewModel.value = arrayAnswers.random().toString()
+                viewModel.questionCurrentViewModel.value = viewModel.arrayQuestions
+                viewModel.answerCurrentViewModel.value = viewModel.arrayAnswers
             }
         }
+        viewModel.questionCurrentViewModel.observe(this@Dz14Fragment as LifecycleOwner, Observer {
+            binding.tvQuestion.text = it
+        })
+        viewModel.answerCurrentViewModel.observe(this@Dz14Fragment as LifecycleOwner, Observer {
+            binding.tvAnswer.text = it
+        })
     }
-
-    private val arrayAnswers: Array<String> = arrayOf(
-        "Да",
-        "Нет",
-        "Возможно",
-        "Скорее всего",
-        "Имееются перспективы",
-        "Вопрос задан не верно"
-    )
-    private val arrayQuestions: Array<String> = arrayOf(
-        "Я устроюсь на работу?",
-        "Меня ждет успех?"
-    )
 
     companion object {
         @JvmStatic
--- app/src/main/java/com/example/tms/dz14/MyViewModel.kt
+++ app/src/main/java/com/example/tms/dz14/MyViewModel.kt
@@ -3,7 +3,23 @@ package com.example.tms.dz14
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class MyViewModel: ViewModel() {
-    val questionViewModel = MutableLiveData<String>()
-    val answerViewModel = MutableLiveData<String>()
+class MyViewModel : ViewModel() {
+    val arrayAnswers: String = arrayOf(
+        "Да",
+        "Нет",
+        "Возможно",
+        "Скорее всего",
+        "Имееются перспективы",
+        "Вопрос задан не верно"
+    ).random().toString()
+    val arrayQuestions: String = arrayOf(
+        "Я устроюсь на работу?",
+        "Меня ждет успех?"
+    ).random().toString()
+    val questionCurrentViewModel: MutableLiveData<String> by lazy {
+        MutableLiveData<String>()
+    }
+    val answerCurrentViewModel: MutableLiveData<String> by lazy {
+        MutableLiveData<String>()
+    }
 }
